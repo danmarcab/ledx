61c2a1ed34a3c67aaf8a11cbca209e2b431fc1ff
Daniel Marin <danmarcab@gmail.com>
2016-02-07 20:49:30 +0000

    improving desing, first implementation of loop

--- lib/ledx/led_controller.ex
+++ lib/ledx/led_controller.ex
@@ -1,30 +1,86 @@
 defmodule Ledx.LedController do
   use GenServer
 
+  defstruct [:name, :module, :config, state: :off, loop_type: :generic, loop: nil]
+
   def start_link(led_name, module, config) do
-    GenServer.start_link(__MODULE__, {module, config}, [name: led_name])
+    GenServer.start_link(__MODULE__, {led_name, module, config}, [name: led_name])
   end
 
-  def init({module, config}) do
-    state = %{module: module, config: config}
+  def init({name, module, config}) do
+    state = %__MODULE__{name: name, module: module, config: config}
     {:ok, state}
   end
 
-  def on(led) do
+  def state(led) do
+    GenServer.call(led, :state)
+  end
+
+  def turn_on(led) do
     GenServer.cast(led, :on)
   end
 
-  def off(led) do
+  def turn_off(led) do
     GenServer.cast(led, :off)
   end
 
-  def handle_cast(:on, %{module: module, config: config} = state) do
-    module.on(config)
-    {:noreply, state}
+  def toggle(led) do
+    GenServer.cast(led, :toggle)
   end
 
-  def handle_cast(:off, %{module: module, config: config} = state) do
-    module.off(config)
-    {:noreply, state}
+  def loop(led, on_off) do
+    loop(led, on_off, on_off)
+  end
+
+  def loop(led, on, off) do
+    GenServer.cast(led, {:loop, on, off})
+  end
+
+  def handle_call(:state, _from, %__MODULE__{state: current_state} = state) do
+    {:reply, current_state, state}
+  end
+
+  def handle_cast(:on, %__MODULE__{} = state) do
+    {:noreply, do_turn_on(state)}
+  end
+
+  def handle_cast(:off, %__MODULE__{} = state) do
+    {:noreply, do_turn_off(state)}
+  end
+
+  def handle_cast(:toggle, %__MODULE__{} = state) do
+    {:noreply, do_toggle(state)}
+  end
+
+  def handle_cast({:loop, on, off}, %__MODULE__{module: module, loop_type: :callback} = state) do
+    {:noreply, module.loop(state, on, off)}
+  end
+
+  def handle_cast({:loop, on, off}, %__MODULE__{module: module, loop_type: :generic} = state) do
+    {:noreply, %{state | loop: %{on: on, off: off}}, 0}
+  end
+
+  def handle_info(:timeout, %__MODULE__{module: module, state: :on, loop: %{off: timeout}} = state) do
+    {:noreply, do_turn_off(state), timeout}
+  end
+
+  def handle_info(:timeout, %__MODULE__{module: module, state: :off, loop: %{on: timeout}} = state) do
+    {:noreply, do_turn_on(state), timeout}
+  end
+
+  defp do_toggle(%__MODULE__{state: :on} = state) do
+    do_turn_off(state)
+  end
+
+  defp do_toggle(%__MODULE__{state: :off} = state) do
+    do_turn_on(state)
+  end
+
+  defp do_turn_on(%__MODULE__{module: module} = state) do
+    %{module.on(state) | state: :on}
+  end
+
+  defp do_turn_off(%__MODULE__{module: module} = state) do
+    %{module.off(state) | state: :off}
   end
 end
--- test/ledx/led_controller_test.exs
+++ test/ledx/led_controller_test.exs
@@ -4,24 +4,58 @@ defmodule Ledx.LedControllerTest do
   alias Ledx.LedController
 
   defmodule TestLed do
-    def on(%{caller: caller}) do
-      send(caller, :on)
+    def on(state) do
+      send(state.config.caller, :on)
+      state
     end
 
-    def off(%{caller: caller}) do
-      send(caller, :off)
+    def off(state) do
+      send(state.config.caller, :off)
+      state
+    end
+
+    def loop_type do
+      :generic
     end
   end
 
-  test "the truth" do
-    {:ok, pid} = LedController.start_link(:test_led, TestLed, %{caller: self})
+  setup do
+    {:ok, _pid} = LedController.start_link(:test_led, TestLed, %{caller: self})
+    :ok
+  end
 
-    LedController.on(:test_led)
-
+  test "turn on/off" do
+    assert LedController.state(:test_led) == :off
+    LedController.turn_on(:test_led)
     assert_receive :on
-
-    LedController.off(:test_led)
-
+    assert LedController.state(:test_led) == :on
+    LedController.turn_off(:test_led)
     assert_receive :off
+    assert LedController.state(:test_led) == :off
+  end
+
+  test "toggle on/off" do
+    assert LedController.state(:test_led) == :off
+    LedController.toggle(:test_led)
+    assert_receive :on
+    assert LedController.state(:test_led) == :on
+    LedController.toggle(:test_led)
+    assert_receive :off
+    assert LedController.state(:test_led) == :off
+  end
+
+  test "loop on/off" do
+    LedController.loop(:test_led, 50, 100)
+    assert_receive :on
+    # assert LedController.state(:test_led) == :on
+    refute_receive :off, 49
+    assert_receive :off, 5
+    # assert LedController.state(:test_led) == :off
+    refute_receive :on, 99
+    assert_receive :on, 5
+    # assert LedController.state(:test_led) == :on
+    refute_receive :off, 49
+    assert_receive :off, 5
+    # assert LedController.state(:test_led) == :off
   end
 end
